92fc5a6c3101b6449e0fc46ff00872e54b03714b
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2026-07-31 11:52:50 +0530

    Add files via upload

---
+++ 8.DFS.py
@@ -0,0 +1,20 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+visited = []
+
+def dfs(node):
+    if node not in visited:
+        print(node, end=" ")
+        visited.append(node)
+        for i in graph[node]:
+            dfs(i)
+
+start = input("Enter starting node: ")
+dfs(start)
