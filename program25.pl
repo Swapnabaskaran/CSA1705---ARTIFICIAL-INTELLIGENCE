eb8009c7167e1fea5f5857e7172837ab08f96ad5
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2026-08-10 11:30:14 +0530

    Add files via upload

---
+++ program25.pl
@@ -0,0 +1,10 @@
+can_get_banana :-
+    monkey_on_floor,
+    box_under_banana,
+    monkey_on_box,
+    monkey_gets_banana.
+
+monkey_on_floor.
+box_under_banana.
+monkey_on_box.
+monkey_gets_banana.
