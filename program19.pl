2335327453424372a0697b61f7f048826445da64
SwapnaB <154415902+Swapnabaskaran@users.noreply.github.com>
2026-08-10 10:48:17 +0530

    Add files via upload

---
+++ program19.pl
@@ -0,0 +1,14 @@
+student(101,swapna).
+student(102,rahul).
+
+teacher(ai,raja).
+teacher(dbms,kumar).
+
+subject(ai,'CSA301').
+subject(dbms,'CSA302').
+
+studies(swapna,ai).
+studies(rahul,dbms).
+
+teaches(raja,ai).
+teaches(kumar,dbms).
